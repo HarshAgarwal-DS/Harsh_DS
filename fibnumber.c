60cc42bb90c11049d3cb7c9a8c950b4b24b3e658
HarshAgarwal-DS <agarwalharsh24092006@gmail.com>
2026-03-02 21:40:03 +0530

    fibnacci number

---
+++ fibnumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void fibonacci(int n)
+{
+    int a = 0, b = 1, c;
+
+
+    for(int i = 2; i < n; i++)
+    {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+}
+
+int main()
+{
+    int n = 10;
+    fibonacci(n);
+    return 0;
+}
